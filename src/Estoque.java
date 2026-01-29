734a9d320d6b4965c9850b8217e5e7ef545b1005
Gabriel B Cunha <gabriel_sampa@outlook.com>
2026-01-28 22:53:56 -0300

    feat: adiciona regras de cadastro, remoção e busca de produtos

--- src/Estoque.java
+++ src/Estoque.java
@@ -1,3 +1,36 @@
-public class Estoque {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Estoque {
+    private int proximoId =1;
+    private final List<Produto> produtos = new ArrayList<>();
+
+    public Produto adicionarProduto(String nome, int preco, int quantidade){
+        int idGerado = proximoId++;
+        Produto produto = new Produto(idGerado, nome, preco, quantidade);
+        produtos.add(produto);
+        return produto;
+    }
+
+    public boolean removerProduto(int id){
+        Produto alvo = buscarProdutoId(id);
+        if(alvo != null){
+            produtos.remove(alvo);
+            return true;
+        }
+        return false;
+    }
+
+    public List<Produto> listarProdutos(){
+        return new ArrayList<>(produtos);
+    }
+
+    public Produto buscarProdutoId(int id){
+        for(Produto produto : produtos){
+            if(produto.getId() == id){
+                return produto;
+            }
+        }
+        return null;
+    }
 }
